bc8ee31d71d4986a682f47a51f30fd52404e142c
henryjxie <henryjxie@gmail.com>
2024-12-28 13:54:30 -0800

    Fix Github Page deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Use '/' for user pages (like threepointfive2627.github.io)
+  base: '/', // Correct base path for your user GitHub Pages (threepointfive2627.github.io)
 });
