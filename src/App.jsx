f5cc19cd5ebf3ffd4fd44db2c7250bf219057567
henryjxie <henryjxie@gmail.com>
2024-12-28 17:45:16 -0800

    Fix Github Page deployment

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Gallery from './gallery'
+
+function App() {
+  return (
+    <>
+      <h1 style={{textAlign: 'center'}}>threepointfive</h1>
+      <Gallery/>  
+    </>
+  )
+}
+
+export default App
